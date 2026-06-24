0436716c4db910bbff391c365653597bc6266c71
HAILEY BENITHA <haileybenitha@gmail.com>
2026-06-24 23:37:00 +0530

    Completed Mockito Exercise 1 - Mocking and Stubbing

---
+++ Week-1/Module4-TDDJUnitMockito/Ex1-MockingAndStubbing/src/test/java/com/cts/mockito/AppTest.java
@@ -0,0 +1,26 @@
+package com.cts.mockito;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+interface ExternalApi {
+    String getData();
+}
+class MyService {
+    private ExternalApi api;
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+    public String fetchData() {
+        return api.getData();
+    }
+}
+public class AppTest {
+    @Test
+    void testExternalApi() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+        assertEquals("Mock Data", result);
+    }
+}
