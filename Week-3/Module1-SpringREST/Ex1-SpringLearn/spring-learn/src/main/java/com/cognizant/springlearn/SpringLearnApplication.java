c2a61729c020588459a2f13c5ef9842cc7ca1dda
HAILEY BENITHA <haileybenitha@gmail.com>
2026-07-01 23:58:47 +0530

    Implemented Spring XML Configuration - Country Bean

--- Week-3/Module1-SpringREST/Ex1-SpringLearn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week-3/Module1-SpringREST/Ex1-SpringLearn/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -17,7 +19,23 @@ public class SpringLearnApplication {
 
         SpringApplication.run(SpringLearnApplication.class, args);
 
+        displayCountry();
+
         LOGGER.info("END");
     }
 
+    public static void displayCountry() {
+
+        LOGGER.info("START");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("END");
+    }
 }
